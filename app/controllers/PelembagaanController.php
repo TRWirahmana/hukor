d0cc4d72649836af3c195c609d61fd344ee51a8b
andijs <jufri.suand@gmail.com>
2014-01-27 14:04:50 +0700

    pelembagaan tabel update

--- app/controllers/PelembagaanController.php
+++ app/controllers/PelembagaanController.php
@@ -88,6 +88,7 @@ class PelembagaanController extends BaseController {
 		$pelembagaan->perihal = Input::get('perihal');
 		$pelembagaan->catatan = Input::get('catatan');
 		$pelembagaan->lampiran = "filename";
+		$pelembagaan->status = Input::get('status');
 
         $pelembagaan->tgl_usulan = Carbon::now();
         $pelembagaan->save();

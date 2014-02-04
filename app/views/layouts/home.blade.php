1038eb420ff91a0babfa3defb494e35db123c181
rulyardiansyah <pendekarkerayon@gmail.com>
2014-02-04 13:38:57 +0700

    welcome bastardo

--- app/views/layouts/home.blade.php
+++ app/views/layouts/home.blade.php
@@ -1,3 +1,9 @@
 @section('content')
+<h2><span class="rulycon-home-2"></span> SELAMAT DATANG</h2>
+
+<div class="content-non-title">
+  <p id="welcome-to-the-fucking-app"><span class="rulycon-accessibility"></span><br/>Selamat datang di aplikasi Hukum &amp; Organisasi <br/> Kementerian Pendidikan &amp; Kebudayaan Republik Indonesia</p>
+</div>
+
 @include('flash')
 @stop

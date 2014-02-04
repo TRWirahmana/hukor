d8dfb1ea952b2ba6bdf6b9bd052fe3cd847b67ed
rulyardiansyah <pendekarkerayon@gmail.com>
2014-02-04 14:06:48 +0700

    pemanis copyright ga penting

--- app/views/layouts/home.blade.php
+++ app/views/layouts/home.blade.php
@@ -2,7 +2,12 @@
 <h2><span class="rulycon-home-2"></span> SELAMAT DATANG</h2>
 
 <div class="content-non-title">
-  <p id="welcome-to-the-fucking-app"><span class="rulycon-accessibility"></span><br/>Selamat datang di aplikasi Hukum &amp; Organisasi <br/> Kementerian Pendidikan &amp; Kebudayaan Republik Indonesia</p>
+  <p id="welcome-to-the-fucking-app">
+    <span class="rulycon-accessibility"></span><br/>
+    Selamat datang di aplikasi Hukum &amp; Organisasi<br/>
+    Kementerian Pendidikan &amp; Kebudayaan Republik Indonesia<br/>
+    <small>© 2013 DIREKTORAT JENDERAL KEBUDAYAAN REPUBLIK INDONESIA</small>
+  </p>
 </div>
 
 @include('flash')

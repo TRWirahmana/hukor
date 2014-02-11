47f681e78e709d8170bb6af8c431610fd2780c8c
egiw <egi.hasdi@sangkuriang.co.id>
2014-02-11 15:56:44 +0700

    ubah pelembagaan

--- app/controllers/PelembagaanController.php
+++ app/controllers/PelembagaanController.php
@@ -20,7 +20,7 @@ class PelembagaanController extends BaseController {
 
 	       	// $listTgl = array("" => "Semua") + Pelembagaan::select(array( DB::raw('DATE_FORMAT(tgl_usulan,"%Y") As usulan_year')))
 	        // 													->lists('usulan_year', 'usulan_year');
-        if($user->role_id == 3){
+  //      if($user->role_id == 3){
 //	       	$listTgl = array("" => "Semua") + Pelembagaan::select(array( DB::raw('DATE_FORMAT(tgl_usulan,"%Y") As usulan_year')))
 //	        													->lists('usulan_year', 'usulan_year');
 
@@ -30,8 +30,7 @@ class PelembagaanController extends BaseController {
         	$this->layout = View::make('layouts.master');
         }
 	    
-	    $this->layout->content = View::make('Pelembagaan.index', array( 'user' => $user, 'status_belum' => $statusUn, 'status_proses' => $statusPro));		
-=======
+//	    $this->layout->content = View::make('Pelembagaan.index', array( 'user' => $user, 'status_belum' => $statusUn, 'status_proses' => $statusPro));		
 	    $this->layout->content = View::make('Pelembagaan.index', array( 'user' => $user));//, array( 'status_belum' => $statusUn, 'status_proses' => $statusPro));
 	}
 

ec40c9e2132c918d93816c7d4f73bc282bd1414a
Sangku Devel <web@sangku-devel2.cloudapp.net>
2014-03-04 10:33:59 +0000

    fix bug kecil

--- app/controllers/BantuanHukumController.php
+++ app/controllers/BantuanHukumController.php
@@ -9,6 +9,7 @@ class BantuanHukumController extends BaseController{
      */
 
     public function index() {
+var_dump('asdf');exit
 	$user = Auth::user()->role_id;
         $all = Menu::all();
         $all->toArray();

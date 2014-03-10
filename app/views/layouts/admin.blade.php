014b13a2c8e593252183594d40b240639048913c
dwinov <erlan.novayanto@sangkuriang.co.id>
2014-03-10 12:14:09 +0700

    text logo

--- app/views/layouts/admin.blade.php
+++ app/views/layouts/admin.blade.php
@@ -87,7 +87,11 @@
             <div class="leftpanel">
                 <div id="logo-wrapper">
                     <img src="{{asset('assets/images/logo-only.png')}}" alt="Kementrian Pendidikan Nasional RI"/>
-                  <h4><span>Layanan Hukum &amp; Organisasi</span><br/><span>Kementerian Pendidikan dan Kebudayaan</span></h4>
+                    <h4>
+                        <span>Biro Hukum dan Organisasi</span>
+                        <span>Kementerian Pendidikan dan Kebudayaan</span><br>
+                        <span>Republik Indonesia</span>
+                    </h4>
                 </div>
                 <div id="welcome-message">
                     @if($user->role_id == 3)
--- app/views/layouts/admin_login.blade.php
+++ app/views/layouts/admin_login.blade.php
@@ -50,7 +50,11 @@
     <div class="loginpanelinner">
         <div class="logo animate3 bounceIn">
           <img class="inputwrapper animate0 bounceIn" src="{{asset('assets/images/logo-only.png')}}" alt=""/>
-          <h4><span>Layanan Hukum &amp; Organisasi</span><br/><span>Kementerian Pendidikan dan Kebudayaan</span></h4>
+            <h4>
+                <span>Biro Hukum dan Organisasi</span><br>
+                <span>Kementerian Pendidikan dan Kebudayaan</span><br>
+                <span>Republik Indonesia</span>
+            </h4>
         </div>
         {{-- form login--}}
         {{ Form::open(array('action' => 'LoginController@signin_admin', 'method' => 'post', 'id'=>'user-sign-in-form',

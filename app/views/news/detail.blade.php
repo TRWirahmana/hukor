e08f661f3303d6510e6314b37588d78628441489
rulyardiansyah <pendekarkerayon@gmail.com>
2014-03-21 10:20:11 +0700

    detail berita

--- app/views/news/detail.blade.php
+++ app/views/news/detail.blade.php
@@ -1,40 +1,29 @@
 @section('news-content')
-<div class="maincontent">
-  <div class="container">
+<div class="maincontent" id="detail-news">
+  <div class="container" style="width: 960px;">
     <!-- News Feed -->
     <div class="row-fluid">
-      <div id="dashboard-left" class="span9">
-        <h3 class="section-title" id="news-feed">Berita</h3>
-        <div class="news-content">
-          <div class="row-fluid">
-            <div class="span9">
-              <h4>{{$berita->judul}}</h4>
-		<br>
-                @if($berita->gambar != null)
-                {{ HTML::image('assets/uploads/berita/' . $berita->gambar) }}
-                @endif
-		<br>
-              <p>{{$berita->berita}}</p>
-              
-            </div>
-            <div class="span3">
-              <?php $date = new DateTime($berita->tgl_penulisan); ?>
-              <span class="date-time pull-right">{{$date->format('d')}}  <span class="date"><?php echo HukorHelper::castMonthToString3($date->format('m'))?></span> {{$date->format('Y')}}</span>
-            </div>
-          </div>
+      <div id="dashboard-left" class="span12">
+        <h3 class="section-title" id="news-feed">{{$berita->judul}}</h3>
+        <div class="news-content news-content-detail">
+          <h6><?php $date = new DateTime($berita->tgl_penulisan); ?>
+          <span class="date-time">{{$date->format('d')}}  <span
+              class="date"><?php echo HukorHelper::castMonthToString3($date->format('m')) ?></span> {{$date->format('Y')}}</span></h6>
+          <br>
+          @if($berita->gambar != null)
+          {{ HTML::image('assets/uploads/berita/' . $berita->gambar) }}
+          @endif
+          <br>
+          <p>{{$berita->berita}}</p>
         </div>
       </div>
-      <!--span9-->
-
-
-      <!--span3-->
     </div>
     <!--row-fluid-->
   </div>
 
 
-    </div>
-    <!--container-->
+</div>
+<!--container-->
 </div>
 <!--maincontent-->
 @section('scripts')

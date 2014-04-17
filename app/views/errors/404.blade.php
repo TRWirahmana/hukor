10010aee9857a449df594a47da95a65de1c4770f
rulyardiansyah <pendekarkerayon@gmail.com>
2014-04-17 17:19:44 +0700

    template halaman error

--- app/views/errors/404.blade.php
+++ app/views/errors/404.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.login')
 @section('content')
-	<a href="/"><img src="{{ asset('assets/img/error-404.png') }}" alt="" id="img_error_404"></a>
+	<!--<a href="/"><img src="{{ asset('assets/img/error-404.png') }}" alt="" id="img_error_404"></a>-->
 @stop
